bc4a578f56973118aa264c7c36a8bf2dab5070a4
blekas10 <nojus.stankevicius@gmail.com>
2023-02-21 16:24:48 +0200

    added swiper

--- client/src/pages/single-product-page/index.tsx
+++ client/src/pages/single-product-page/index.tsx
@@ -1,12 +1,28 @@
-import { Box } from '@mui/material';
-import routes from 'navigation/routes';
 import React from 'react';
-import { Navigate, useParams } from 'react-router-dom';
+import {
+  Box, IconButton, Stack, styled,
+} from '@mui/material';
+import routes from 'navigation/routes';
+import { useParams, Navigate } from 'react-router-dom';
 import ApiService from 'services/api-service';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import Img from 'components/ui/img';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+
+const StyledSwiper = styled(Swiper)({
+  width: '100%',
+  height: '100%',
+});
 
 const SingleProductPage = () => {
+  const leftArrowRef = React.useRef<HTMLButtonElement | null>(null);
+  const rightArrowRef = React.useRef<HTMLButtonElement | null>(null);
   const { id } = useParams();
-
   const [product, setProduct] = React.useState<undefined | ProductModel>(undefined);
 
   React.useEffect(() => {
@@ -19,11 +35,62 @@ const SingleProductPage = () => {
   }, []);
 
   if (id === undefined) return <Navigate to={routes.HomePage} />;
+  if (product === undefined) return null;
 
   return (
-    <Box component="pre">
-      {JSON.stringify(product, null, 4)}
-
+    <Box>
+      <Box component="pre">
+        {JSON.stringify(product, null, 4)}
+      </Box>
+      <Box sx={{
+        width: 600,
+        height: 400,
+        margin: 'auto',
+        position: 'relative',
+      }}
+      >
+        <StyledSwiper
+          modules={[Pagination, Navigation]}
+          pagination={{ dynamicBullets: true }}
+          navigation={{
+            enabled: true,
+            nextEl: rightArrowRef.current,
+            prevEl: leftArrowRef.current,
+          }}
+        >
+          {product.images.map((img) => (
+            <SwiperSlide key={img}>
+              <Img src={img} sx={{ width: 1, height: 1 }} />
+            </SwiperSlide>
+          ))}
+        </StyledSwiper>
+        <Stack sx={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          left: 0,
+          zIndex: 3000,
+          justifyContent: 'center',
+        }}
+        >
+          <IconButton ref={leftArrowRef}>
+            <ArrowCircleLeftIcon sx={{ color: 'primary.main', fontSize: 30 }} />
+          </IconButton>
+        </Stack>
+        <Stack sx={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          right: 0,
+          zIndex: 3000,
+          justifyContent: 'center',
+        }}
+        >
+          <IconButton ref={rightArrowRef}>
+            <ArrowCircleRightIcon sx={{ color: 'primary.main', fontSize: 30 }} />
+          </IconButton>
+        </Stack>
+      </Box>
     </Box>
   );
 };
